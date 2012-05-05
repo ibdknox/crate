3926cf49c7373d01207e71160d43d1149f15437c
Chris Granger <ibdknox@gmail.com>
2012-05-04 22:54:34 -0700

    alpha1
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,2 @@
-(defproject crate "0.2.0-snapshot"
-  :description "A ClojureScript implementation of Hiccup"
-  :dependencies [[clojure "1.3.0"]])
+(defproject crate "0.2.0-alpha1"
+  :description "A ClojureScript implementation of Hiccup")
