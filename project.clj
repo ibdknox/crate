c618980868f5a0d2ec18abc1d6430bc839cbfae9
sto <Son.C.To@gmail.com>
2015-11-02 16:07:56 -0500

    rename crate to krate so that I can publish  this on clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crate "0.2.5-SNAPSHOT"
+(defproject krate "0.2.5-SNAPSHOT"
   :url "https://github.com/sonwh98/crate"
   :description "A ClojureScript implementation of Hiccup. Fork of https://github.com/ibdknox/crate"
   :license "Eclipse Public License")
