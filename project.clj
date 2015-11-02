09137e852c53f64f4c9b3dbe49b629caf883bd37
sto <Son.C.To@gmail.com>
2015-11-02 13:23:25 -0500

    url, license info

--- project.clj
+++ project.clj
@@ -1,2 +1,4 @@
 (defproject crate "0.2.5-SNAPSHOT"
-  :description "A ClojureScript implementation of Hiccup")
+  :url "https://github.com/sonwh98/crate"
+  :description "A ClojureScript implementation of Hiccup. Fork of https://github.com/ibdknox/crate"
+  :license "Eclipse Public License")
